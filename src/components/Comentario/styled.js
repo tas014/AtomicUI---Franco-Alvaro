80f3ba41288b0d0d064d877ffca16f87cd8780ad
AlvaRamirez <55244792+AlvaRamirez@users.noreply.github.com>
2020-10-07 20:19:12 -0300

    color qsyo

--- src/components/Comentario/styled.js
+++ src/components/Comentario/styled.js
@@ -3,7 +3,8 @@ import styled from 'styled-components';
 const Coment = styled.p`
 font-family: monospace;
     font-size: 14px;
-    color: green;
+    color: #805300bf;
+    font-weight: bold;
     padding-top: 35px;
 `
 
