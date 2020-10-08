74af9212cce4f710e9266adab80ef2869f8a0e5e
tas014 <francoapicco@gmail.com>
2020-10-08 10:00:45 -0300

    Fixed click

--- src/pages/experiment.js
+++ src/pages/experiment.js
@@ -29,10 +29,10 @@ const Experiment = () => {
                             <Paragraph>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</Paragraph>
                         </Article>
                     </div>
-                    <Card onClick={() => setShown(!shown)}>FARP</Card>
+                    <Card action={() => setShown(!shown)}>FARP</Card>
                     <SlideArticle isShown={shown}>
                         <div id="back">
-                          
+
                         </div>
                         <Paragraph>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</Paragraph>
                     </SlideArticle>
